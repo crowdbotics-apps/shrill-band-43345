55ca43ff3331e0cccbce7144e50dd2996d1b2b70
Crowdbotics <team@crowdbotics.com>
2023-09-12 04:43:53 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "shrill-band-43345",
+  url: "https://shrill-band-43345.botics.co",
+  api: "https://shrill-band-43345.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
